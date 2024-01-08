171db6435a7201358921457a35157631fcd7c276
SebasBakker <sebas.bakker@student.fontys.nl>
2024-01-08 15:51:25 +0100

    Update launchevent.js

--- launchevent.js
+++ launchevent.js
@@ -1,3 +1,66 @@
+/*
+* Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+* See LICENSE in the project root for license information.
+*/
+
+// Add start-up logic code here, if any.
+Office.onReady();
+
+function onNewMessageComposeHandler(event) {
+  const item = Office.context.mailbox.item;
+  const signatureIcon = "iVBORw0KGgoAAAANSUhEUgAAACcAAAAnCAMAAAC7faEHAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAzUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKMFRskAAAAQdFJOUwAQIDBAUGBwgI+fr7/P3+8jGoKKAAAACXBIWXMAAA7DAAAOwwHHb6hkAAABT0lEQVQ4T7XT2ZalIAwF0DAJhMH+/6+tJOQqot6X6joPiouNBo3w9/Hd6+hrYnUt6vhLcjEAJevVW0zJxABSlcunhERpjY+UKoNN5+ZgDGu2onNz0OngjP2FM1VdyBW1LtvGeYrBLs7U5I1PTXZt+zifcS3Icw2GcS3vxRY3Vn/iqx31hUyTnV515kdTfbaNhZLI30AceqDiIo4tyKEmJpKdP5M4um+nUwfDWxAXdzqMNKQ14jLdL5ntXzxcRF440mhS6yu882Kxa30RZcUIjTCJg7lscsR4VsMjfX9Q0Vuv/Wd3YosD1J4LuSRtaL7bzXGN1wx2cytUdncDuhA3fu6HPTiCvpQUIjZ3sCcHVbvLtbNTHlysx2w9/s27m9gEb+7CTri6hR1wcTf2gVf3wBRe3CMbcHYvTODkXhnD0+178K/pZ9+n/C1ru/2HAPwAo7YM1X4+tLMAAAAASUVORK5CYII=";
+
+  // Get the sender's account information.
+  item.from.getAsync((result) => {
+    if (result.status === Office.AsyncResultStatus.Failed) {
+      console.log(result.error.message);
+      event.completed();
+      return;
+    }
+
+    // Create a signature based on the sender's information.
+    const name = result.value.displayName;
+    const options = { asyncContext: name, isInline: true };
+    item.addFileAttachmentFromBase64Async(signatureIcon, "signatureIcon.png", options, (result) => {
+      if (result.status === Office.AsyncResultStatus.Failed) {
+        console.log(result.error.message);
+        event.completed();
+        return;
+      }
+
+      // Add the created signature to the message.
+      const signature = "testestestesteste";
+      item.body.setSignatureAsync(signature, { coercionType: Office.CoercionType.Html }, (result) => {
+        if (result.status === Office.AsyncResultStatus.Failed) {
+          console.log(result.error.message);
+          event.completed();
+          return;
+        }
+
+        // Show a notification when the signature is added to the message.
+        // Important: Only the InformationalMessage type is supported in Outlook mobile at this time.
+        const notification = {
+          type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+          message: "Company signature added.",
+          icon: "none",
+          persistent: false
+        };
+        item.notificationMessages.addAsync("signature_notification", notification, (result) => {
+          if (result.status === Office.AsyncResultStatus.Failed) {
+            console.log(result.error.message);
+            event.completed();
+            return;
+          }
+
+          event.completed();
+        });
+      });
+    });
+  });
+}
+
+
+
 // ==========================
 //   Event Based Activation
 // ==========================
@@ -24,7 +87,7 @@
 
 // TODO: it seems OfficeRuntime.auth.getAccessToken is now supported in event based activation. we may be able to sign in before even opening the addin.
 // https://learn.microsoft.com/en-us/office/dev/add-ins/outlook/use-sso-in-event-based-activation
-Office.onReady();
+
 // ========================
 //   Initialize Libraries
 // ========================
@@ -75,21 +138,9 @@ Office.initialize = function () {
 };
 
 
+function onNewMessageComposeHandler(event) {
 
-function onNewMessageComposeHandler(eventObj) {
-// bind an event on message compose, which will insert the users default signature
-    eformity.hostinfo.initialize();
-
-    // for now, only allow Event Based Activation only in Office Online and not in Mac or Desktop
-    //var diagnostics = Office.context.diagnostics;
-    //if (diagnostics.platform !== 'OfficeOnline') {
-    //    return;
-    //}
-
-    eformity.office.outlook.insertDefaultSignature(function (result) {
-        result.completed();
-    }, eventObj);
-});
+}
 
 
 // ==============================
@@ -1792,10 +1843,3 @@ if (Object.freeze) {
     Object.freeze(AttachmentType);
     Object.freeze(ComposeType);
 }
-
-/*
-* Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-* See LICENSE in the project root for license information.
-*/
-
-/
