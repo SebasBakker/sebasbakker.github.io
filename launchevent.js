8f01537aee9ac670ed00688a6a1682020fa5ba71
SebasBakker <sebas.bakker@student.fontys.nl>
2024-01-15 14:44:41 +0100

    Update launchevent.js

--- launchevent.js
+++ launchevent.js
@@ -93,6 +93,25 @@ _bindAction('onMessageComposeHandler', function (eventObj) {
           event.completed();
           return;
         }
+          
+        // Show a notification when the signature is added to the message.
+        // Important: Only the InformationalMessage type is supported in Outlook mobile at this time.
+        const notification = {
+          type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+          message: "Company signature added.",
+          icon: "none",
+          persistent: false
+        };
+        item.notificationMessages.addAsync("signature_notification", notification, (result) => {
+          if (result.status === Office.AsyncResultStatus.Failed) {
+            console.log(result.error.message);
+            event.completed();
+            return;
+          }
+
+          event.completed();
+        });
+      })
 });
 
 function onNewMessageComposeHandler(event) {
